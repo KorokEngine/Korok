ebfc7499633b4cc7b9df17917536bce09f430a64
ntop <ntop.liu@gmail.com>
2017-10-14 20:52:28 +0800

    add vorbis decoder

--- audio/ogg/decoder_vorbis.go
+++ audio/ogg/decoder_vorbis.go
@@ -1,80 +1,127 @@
 package ogg
 
 import (
-	"korok/audio/codec"
-	"io"
-	"github.com/jfreymuth/oggvorbis"
+	"io/ioutil"
+	"os"
+
+	"vorbis"
+	"unsafe"
+	"log"
 )
 
-/**
-	based on freymuth/oggvorbis
+/// 此处是双通道的代码示例！！
+//func Read(b []byte) (int, error) {
+//	buf := make([]float32, len(b)/2)
+//	n, err := s.reader.Read(buf)
+//	if err != nil {
+//		return -1, err
+//	}
+//	for i := 0; i < n; i++ {
+//		v := int16(buf[i] * (1<<15 - 1))
+//		b[i*2 + 0] = uint8(v)
+//		b[i*2 + 1] = uint8(v>>8)
+//	}
+//	return n*2, nil
+//}
+
+/** impl:
+type Decoder interface {
+	FullDecode() (d []byte, numChan, freq int32, err error)
+
+	Decode() int
+	NumOfChan() int
+	BitDepth() int
+	SampleRate() int32
+	Static() []byte
+	ReachEnd() bool
+}
  */
-type stream struct {
-	reader *oggvorbis.Reader
-}
-
-func (s*stream)Seek(offset int64, whence int) (int64, error) {
-	return 0, nil
-}
-
-func (s*stream)Close() error {
-	return nil
-}
-
-func (s*stream)Read(b []byte) (int, error) {
-	buf := make([]float32, len(b)/2)
-	n, err := s.reader.Read(buf)
-	if err != nil {
-		return -1, err
-	}
-	for i := 0; i < n; i++ {
-		v := int16(buf[i] * (1<<15 - 1))
-		b[i*2 + 0] = uint8(v)
-		b[i*2 + 1] = uint8(v>>8)
-	}
-	return n*2, nil
-}
-
-
-type Audio struct {
-	f *codec.Format
-	s *stream
-}
-
-func (a *Audio) Format() *codec.Format {
-	return a.f
-}
-
-func (a *Audio) Stream() codec.Stream {
-	return a.s
-}
-
-// impl Decode(r io.Reader) (codec.Audio, error)
 type Decoder struct {
+	numChannels   int32
+	sampleRate    int32
+	bitDepth      int32
+
+	buffer []byte
+	name string
+	vorb *vorbis.Vorbis
 }
 
-// Read returns raw wav data from an input reader
-func (*Decoder)Decode(r io.Reader) (codec.Audio, error) {
-	reader, err := oggvorbis.NewReader(r)
+func (d *Decoder) NumOfChan() int32 {
+	return d.numChannels
+}
+
+func (d *Decoder) BitDepth() int32 {
+	return d.bitDepth
+}
+
+func (d *Decoder) SampleRate() int32 {
+	return d.sampleRate
+}
+
+func (d *Decoder) Buffer() []byte {
+	return d.buffer
+}
+
+func (d *Decoder) ReachEnd() bool {
+	return false
+}
+
+func (*Decoder) FullDecode(file *os.File) (data []byte, numChan, bitDepth, freq int32, err error) {
+	b, err := ioutil.ReadAll(file)
+	defer file.Close()
+
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	f := codec.Format{
-		NumChannels:reader.Channels(),
-		SampleRate:reader.SampleRate(),
-		BitDepth:reader.Bitrate().Nominal, // TODO
-	}
+	_data, _numChan, _freq, err := vorbis.Decode(b)
 
-	s := stream{reader}
-
-	a := &Audio{
-		f: &f,
-		s: &s,
+	if err != nil {
+		return
 	}
-	return a, nil
+	ptr := unsafe.Pointer(&_data[0])
+	data = ((*[1<<20]byte)(ptr))[:len(_data)*2]
+	numChan = int32(_numChan)
+	bitDepth = 16
+	freq = int32(_freq)
+	return
 }
 
-func init() {
-	codec.RegisterFormat("ogg", "vorbis", &Decoder{})
+func (d *Decoder) Decode() int {
+	if d.vorb == nil {
+		f, err := os.Open(d.name)
+		if err != nil {
+			return 0
+		}
+		v, err := vorbis.New(f)
+		if err != nil {
+			return 0
+		}
+		d.vorb = v
+		d.numChannels = int32(v.Channels)
+		d.sampleRate  = int32(v.SampleRate)
+		d.bitDepth = 16
+	}
+
+	data, err := d.vorb.Decode()
+	if err != nil {
+		return 0
+	}
+	log.Println("decode data:", len(data))
+
+	ptr := unsafe.Pointer(&data[0])
+	d.buffer = ((*[1<<20]byte)(ptr))[:len(data)*4]
+	return len(data) * 4
+}
+
+func (d *Decoder) Close() {
+	if v := d.vorb; v != nil {
+		v.Close()
+	}
+}
+
+func NewVorbisDecoder(name string) (d *Decoder, err error){
+	d = new(Decoder)
+	d.name = name
+	return
 }
