783d539242836762aeff4163174b890791f11573
ntop <ntop.liu@gmail.com>
2017-11-04 21:50:21 +0800

    manage table in game.DB

--- game/game.go
+++ game/game.go
@@ -3,18 +3,26 @@ package game
 import (
 	timer "time"
 
-
 	"github.com/go-gl/glfw/v3.2/glfw"
-	"github.com/go-gl/mathgl/mgl32"
 
 	"korok/gfx"
-	"korok/gfx/text"
 	"korok/assets"
 	"korok/physics"
-	"korok/anim/spine"
 	"korok/anim"
 	"korok/audio"
+	"korok/engi"
 )
+
+type Table interface {}
+
+type DB struct {
+	EntityM *engi.EntityManager
+	Tables []Table
+}
+
+func (db DB) Add(t interface{}) {
+	db.Tables = append(db.Tables, t)
+}
 
 // 统一管理游戏各个子系统的创建和销毁的地方
 var G *Game
@@ -22,23 +30,14 @@ var scenes = make(map[string]Scene)
 var current Scene
 
 type Game struct {
+	DB
+
 	*gfx.RenderSystem
 	*physics.CollisionSystem
 	*anim.AnimationSystem
 	*audio.AudioSystem
 
 	State int
-
-	Label *text.LabelComp
-
-	Font uint32
-
-	skeleton *spine.Skeleton
-	run *spine.Animation
-	jump *spine.Animation
-	hit  *spine.Animation
-	death *spine.Animation
-	skRender *anim.SkeletonRender
 }
 /// window callback
 func (g *Game) OnCreate() {
@@ -79,26 +78,11 @@ func (g *Game) Init()  {
 	g.CollisionSystem = physics.NewCollisionSystem()
 
 	// animation
-	g.AnimationSystem = anim.NewAnimationSystem()
 
 	/// Customized scene
 	if current != nil {
 		current.Preload()
 		current.Setup(g)
-	}
-
-	// init text
-	{
-		assets.LoadFont("assets/font/font.png", "assets/font/font.json")
-		font := assets.GetFont("assets/font/font.json")
-		g.Font = font.Texture
-
-		label := text.NewText(font)
-		label.SetString("Hello, %s", "Korok!")
-		//label.Scale = 10
-		//label.Position = mgl32.Vec2{50, 50}
-		label.Color = mgl32.Vec3{0, 0, 0}
-		g.Label = label
 	}
 }
 
@@ -138,17 +122,3 @@ func (g *Game) Update()  {
 
 func (g *Game) Draw(dt float32)  {
 }
-
-
-type TextureLoader struct {
-}
-
-func (*TextureLoader) Load(page *spine.AtlasPage) error {
-	return nil
-}
-
-func (*TextureLoader) Unload(page *spine.AtlasPage) error {
-	return nil
-}
-
-
--- korok.go
+++ korok.go
@@ -8,6 +8,10 @@ import (
 	"korok/engi"
 	"korok/game"
 	"korok/hid"
+	"korok/gfx/dbg"
+	"korok/particle"
+	"korok/anim"
+	"korok/physics"
 )
 
 const VERSION_CODE  = 1
@@ -23,6 +27,32 @@ func Run(options *Options)  {
 
 	g := &game.Game{}
 	G = g
+	Entity = g.DB.EntityM
+
+	for _, table := range g.DB.Tables {
+		switch t := table.(type) {
+		case *gfx.SpriteTable:
+			Sprite = t
+		case *gfx.MeshTable:
+			Mesh = t
+		case *gfx.TransformTable:
+			Transform = t
+		case *gfx.TextTable:
+			Text = t
+		case *particle.ParticleSystemTable:
+			ParticleSystem = t
+		case *anim.SkeletonTable:
+			Skeleton = t
+		case *physics.RigidBodyTable:
+			RigidBody = t
+		case *physics.ColliderTable:
+			Collider = t
+		case *game.TagTable:
+			Tag = t
+		case *game.ScriptTable:
+			Script = t
+		}
+	}
 
 	hid.RegisterWindowCallback(g)
 	hid.CreateWindow(&hid.WindowOptions{
@@ -34,7 +64,7 @@ func Run(options *Options)  {
 
 func SetDebug(enable bool) {
 	if enable == false {
-		log.SetOutput(ioutil.Discard)
+		dbg.SetOutput(ioutil.Discard)
 	}
 }
 var G *game.Game
@@ -42,9 +72,23 @@ var G *game.Game
 ///// entity-api
 var Entity *engi.EntityManager
 
+var Script *game.ScriptTable
+var Tag    *game.TagTable
+
 ///// shortcut component-api for rendering system
 var Sprite 	   *gfx.SpriteTable
 var Mesh       *gfx.MeshTable
 var Transform  *gfx.TransformTable
+var Text       *gfx.TextTable
+
+///// particle system
+var ParticleSystem *particle.ParticleSystemTable
+
+///// animation
+var Skeleton       *anim.SkeletonTable
+
+///// physics
+var RigidBody *physics.RigidBodyTable
+var Collider  *physics.ColliderTable
 
 
