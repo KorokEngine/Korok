cc417677492e8f2f46cf154aaf52b9c29bfb5d30
ntop <ntop.liu@gmail.com>
2018-02-20 21:37:49 +0800

    layout concept proof..

--- gui/gui_lib.go
+++ gui/gui_lib.go
@@ -6,6 +6,8 @@ import (
 	"korok.io/korok/gfx"
 	"korok.io/korok/gfx/bk"
 	"korok.io/korok/hid/input"
+
+	"log"
 )
 
 type EventType uint8
@@ -13,7 +15,9 @@ const (
 	EventNone EventType = iota
 	EventDown
 	EventUp
-	EventMove
+	EventStartDrag
+	EventEndDrag
+	EventDragging
 )
 
 // 一个Context维护UI层逻辑:
@@ -24,56 +28,94 @@ const (
 // 在窗口内绘制的UI会受到窗口的管理.
 type Context struct {
 	DrawList
-	Layout
+	Layout LayoutManager
 	*Style
 
 	// ui global state
 	state struct{
 		hot, active int
 		mouseX, mouseY, mouseDown int
+
+		// drag state
+		draggingPointer int
+		draggingStart mgl32.Vec2
+
+		isLastEventPointerType bool
+		pointerCapture int
 	}
+
+	// sqNum should be same for  layout and drawing
+	sqNum int
 }
 
 func NewContext(style *Style) *Context {
 	c := &Context{
 		Style:style,
 	}
+	c.state.draggingPointer = -1
+	c.state.isLastEventPointerType = false
+	c.state.pointerCapture = -1
 	c.DrawList.Initialize()
+	c.Layout.Initialize()
 	return c
 }
 
 // Widgets: Text
-func (ctx *Context) Text(text string, style *TextStyle) (id int){
-	x,y := Gui2Game(ctx.Bound.X, ctx.Bound.Y)
+// 渲染阶段，取出大小，计算位置开始渲染
+// Layout: 1 2 3 4 ....
+// Draw:   ^ ^ ^ ^ ....
+// 布局阶段仅计算出大小
+//
+func (ctx *Context) Text(id ID, text string, style *TextStyle)  *Bound {
+	lm := &ctx.Layout
+	if bb, ok := lm.Element(id); ok {
+		g := lm.hGroup
 
-	var font gfx.FontSystem
-	var fontSize float32
-	var color uint32
-	var wrapWidth = ctx.Bound.W
 
-	dft := &ctx.Style.Text
-	if style == nil {
-		font = dft.Font
-		fontSize = dft.Size
-		color = dft.Color
+		// x, y = margin + group + group-offset
+		// todo 暂时不处理 margin 的问题，现在 只能在 bound 的xy字段记录
+		// todo top/left margin 无法记录 bottom/right 的margin
+		x,y := Gui2Game(g.X + lm.Bound.X, g.Y + lm.Bound.Y)
+
+
+		var (
+			font = style.Font
+			fontSize = style.Size
+			color = style.Color
+			wrapWidth = bb.W + 10
+		)
+		ctx.DrawList.AddText(mgl32.Vec2{x, y}, text, font, fontSize, color, wrapWidth)
+
+		// 每绘制完一个元素都要偏移一下光标
+		ctx.Layout.Advance(bb)
+		lm.Extend(bb)
+
+		return bb
 	} else {
-		if style.Font == nil {
-			font = dft.Font
-		}
-		if style.Size == 0 {
-			fontSize = dft.Size
-		}
-		if style.Color == 0 {
-			color = dft.Color
-		}
-		if style.Lines == 1 {
-			wrapWidth = 0
-		}
-	}
+		bb := lm.NewElement(id)
+		size := ctx.CalcTextSize(text,0, style.Font, style.Size)
 
-	size := ctx.DrawList.AddText(mgl32.Vec2{x, y}, text, font, fontSize, color, wrapWidth)
-	id = ctx.Layout.Push(&Bound{ctx.Bound.X, ctx.Bound.Y, size[0], size[1]})
-	return
+		// 加入 Cursor的偏移位置
+		// 这样不太好
+		bb.W, bb.H = size[0], size[1]
+		bb.X, bb.Y = lm.Bound.Offset.X, lm.Bound.Offset.Y
+
+		lm.Extend(bb)
+		return bb
+	}
+}
+
+// 1. LayoutManager pass - 计算出各个控件的大小..
+func (ctx *Context) TextLayout(id ID, text string, style *TextStyle) {
+
+	// 计算大小
+	size := ctx.CalcTextSize(text,0, style.Font, style.Size)
+	bb := ctx.Layout.Bound
+	bb.W, bb.H = size[0], size[1]
+
+	// 计算绝对位置
+	//g := ctx.Layout.Group()
+	//bb.X, bb.Y = g.X, g.Y
 }
 
 func (ctx *Context) CalcTextSize(text string, wrapWidth float32, font gfx.FontSystem, fontSize float32) mgl32.Vec2 {
@@ -85,7 +127,7 @@ func (ctx *Context) CalcTextSize(text string, wrapWidth float32, font gfx.FontSy
 }
 
 // Widgets: InputEditor
-func (ctx *Context) InputText(hint string, lyt Layout, style *InputStyle) {
+func (ctx *Context) InputText(hint string, lyt LayoutManager, style *InputStyle) {
 
 }
 
@@ -98,7 +140,6 @@ func (ctx *Context) Image(texId uint16, uv mgl32.Vec4, style *ImageStyle) (id in
 			bound.W, bound.H = tex.Width, tex.Height
 		}
 	}
-	id = ctx.Layout.Push(&bound)
 	max := min.Add(mgl32.Vec2{bound.W, bound.H})
 	var color uint32
 	if style != nil {
@@ -134,7 +175,7 @@ func (ctx *Context) Button(text string, style *ButtonStyle) (id int, event Event
 	w, h := textSize[0]+20, textSize[1]+20
 
 	// Check Event
-	event = ctx.CheckEvent(&Bound{x, y, w, h})
+	event = ctx.CheckEvent(&Bound{0, x, y, w, h}, false)
 
 	// Render Frame
 	ctx.renderFrame(x, y, w, h, color, rounding)
@@ -142,11 +183,11 @@ func (ctx *Context) Button(text string, style *ButtonStyle) (id int, event Event
 	// Render Text
 	x = bound.X + w/2 - textSize[0]/2
 	y = bound.Y + h/2 - textSize[1]/2
-	ctx.renderTextClipped(text, &Bound{x, y, 0, 0}, &style.TextStyle)
+	ctx.renderTextClipped(text, &Bound{0, x, y, 0, 0}, &style.TextStyle)
 
-	// push bound
-	bound.W, bound.H = w, h
-	id = ctx.Layout.Push(&bound)
+	// push bound todo refactor button bounds
+	//bound.W, bound.H = w, h
+	// id = ctx.Layout.Push(&bound)
 	return
 }
 
@@ -171,47 +212,97 @@ func (ctx *Context) EventBackground(event EventType) {
 // 现在只检测一个点, 通常是鼠标的左键或者是多点触控时的第一个手指的位置
 // 这样可以记录当前控件的状态...
 // 如何根据状态绘制？？
-func (ctx *Context) CheckEvent(bound *Bound) EventType {
+func (ctx *Context) CheckEvent(bound *Bound, checkDragOnly bool) EventType {
 	event := EventNone
-	if p := input.PointerPosition(0); bound.InRange(p.MousePos) {
+
+	// log.Println("check scope bouds:", bound, " point:",  input.PointerPosition(0).MousePos)
+
+	if p := input.PointerPosition(0); bound.InRange(p.MousePos) || ctx.state.pointerCapture == 123{
 		btn := input.PointerButton(0)
 		id := int(0) // // todo 设计ID系统，记录每个按键的位置..
-		if btn.JustPressed() {
-			ctx.state.active = id
-			event = EventDown
+
+		// todo finger id!!!
+		// 进入此处的条件为：有正确的手指头，手指头说明已经处于 drag 状态！！
+		// 这个地方只有 drag 能进
+		if ctx.state.draggingPointer == 0 && !btn.JustPressed() {
+			// The pointer is in drag operation
+			if btn.JustReleased() {
+				event = EventEndDrag
+				log.Println("drag end real..", event)
+				ctx.state.draggingPointer = -1
+				ctx.state.draggingStart = mgl32.Vec2{}
+			} else if btn.Down() {
+				event = EventDragging
+			} else {
+				ctx.state.draggingPointer = -1
+			}
+		} else {
+			if !checkDragOnly {
+				if btn.JustPressed() {
+					ctx.state.active = id
+					event = EventDown
+				}
+				if btn.JustReleased() && ctx.state.active == id {
+					event = EventUp
+				}
+			}
+
+			// Check for drag events
+
+			// 1. 此时未必是 drag，但是记下位置，接下来根据位移来得到是否是drag操作
+			if btn.JustPressed() {
+				ctx.state.draggingStart = p.MousePos
+				log.Println("just pressed!!")
+			}
+			// 2. 如果接下来的移动超出了阈值，那么判断为 drag 操作
+			// 超出初始按下位置周围20像素，认为是 drag 操作, 否则可能依然是点击（只是手滑了一下）
+			if btn.Down() && bound.InRange(ctx.state.draggingStart) {
+				startPosition := ctx.state.draggingStart
+				dragThreshHold := float32(10)
+				bb := Bound{0,startPosition[0]-dragThreshHold, startPosition[1]-dragThreshHold, 8, 8}
+				//
+				if !bb.InRange(p.MousePos) {
+					event = EventStartDrag
+					ctx.state.draggingStart = p.MousePos
+					ctx.state.draggingPointer = 0
+
+					log.Println("drag start real ..")
+				}
+			}
+
+			if event > 0 {
+				ctx.state.isLastEventPointerType = true
+			}
 		}
-		if btn.JustReleased() && ctx.state.active == id {
-			event = EventUp
-		}
+
 	}
 	return event
 }
 
-func (ctx *Context) ImageButton(texId uint16, lyt Layout, style *ImageButtonStyle) EventType{
+func (ctx *Context) ImageButton(texId uint16, lyt LayoutManager, style *ImageButtonStyle) EventType{
 	return EventNone
 }
 
 func (ctx *Context) Rect(w, h float32, style *RectStyle) (id int){
-	bb := ctx.Bound
+	bb := ctx.Layout.Bound
 
 	x, y := Gui2Game(bb.X, bb.Y)
 
 	var min, max mgl32.Vec2
 
-	if ctx.Horizontal == Left2Right {
+	if ctx.Layout.Horizontal == Left2Right {
 		min[0], max[0] = x, x+w
 	} else {
 		min[0], max[0] = x-w, x
 	}
 
-	if ctx.Vertical == Top2Bottom {
+	if ctx.Layout.Vertical == Top2Bottom {
 		min[1], max[1] = y-h, y
 	} else {
 		min[1], max[1] = y, y+h
 	}
 
 	bb.W, bb.H = w, h
-	id = ctx.Layout.Push(&bb)
 
 	if style == nil {
 		style = &ctx.Style.Rect
@@ -237,6 +328,129 @@ func (ctx *Context) renderFrame(x, y, w, h float32, fill uint32, rounding float3
 	//log.Println("renderFrame:",min, max, fill, rounding)
 }
 
+// draw line rect
+func (ctx *Context) RenderBorder(x, y, w, h float32, color uint32) {
+	x, y = Gui2Game(x, y)
+	min := mgl32.Vec2{x, y-h}
+	max := mgl32.Vec2{x+w, y}
+
+	ctx.DrawList.AddRect(min, max, color, 0, 0, 0.5)
+}
+
+// Slider 的绘制很简单，分别绘制滑动条和把手即可
+// 难点在于跟踪把手的滑动距离
+// Slider的风格，没有想好怎么控制，暂时使用两张图片
+// 分别绘制Bar和Knob
+// Slider 需要保存混动的结果，否则
+func (ctx *Context) Slider(value float32, style *SliderStyle) (v1 float32){
+	bb := ctx.Layout.Bound
+
+	x, y := Gui2Game(bb.X, bb.Y)
+	w, h := bb.W, bb.H
+
+	//var bar, knob uint16
+	//if style == nil {
+	//	bar, knob = ctx.Style.Slider.Bar, ctx.Style.Slider.Knob
+	//} else {
+	//	bar, knob = style.Bar, style.Knob
+	//}
+	//
+	//min, max := mgl32.Vec2{x, y}, mgl32.Vec2{x+w, y+h}
+	//ctx.DrawList.AddImageNinePatch(bar, min, max, mgl32.Vec2{0, 0}, mgl32.Vec2{1, 1}, mgl32.Vec4{.5, .5, .5, .5}, 0xFFFFFFFF)
+	//
+	//
+	//ctx.DrawList.AddImage()
+	// Slider 需要设定一些自定义的属性，目前没有想好如何实现，先把逻辑实现了
+	// 用两种颜色来绘制
+
+	// 说明滑动了，那么应该使用最新的值，而不是传入的值
+	if v := ctx.checkSlider(&bb.Bound); v != 0 {
+		value = v
+		v1 = v
+	}
+
+	min, max := mgl32.Vec2{x, y-h}, mgl32.Vec2{x+w, y}
+	ctx.DrawList.AddRectFilled(min, max, 0xFFCDCDCD, 5, FlagCornerAll)
+
+	centre := mgl32.Vec2{x+w*value, y-h/2}
+	ctx.DrawList.AddCircleFilled(centre, 10, 0xFFABABAB, 12)
+
+	return
+}
+
+// Scroll 效果的关键是使用裁切限制滚动区域，然后
+// 通过计算拖拽，来得到争取的偏移
+func (ctx *Context) StartScroll(size, offset mgl32.Vec2) {
+	event := ctx.CheckEvent(nil, false)
+
+	if event == EventStartDrag {
+		ctx.capturePoint()
+	} else if event == EventEndDrag {
+		ctx.releasePointer()
+	}
+	// 好像算法也不是很难
+}
+
+func (ctx *Context) EndScroll() {
+	//
+}
+
+
+
+// 这里的实现基于拖拽的实现，所以
+// 只要正确的实现了拖拽，这里的就可以很容易的实现
+func (ctx *Context) checkSlider(bound *Bound) (v float32){
+	event := ctx.CheckEvent(bound, false)
+
+	// log.Println("returrn evnt:", event == EventEndDrag)
+
+	if event == EventStartDrag {
+		ctx.state.pointerCapture = 123
+
+		log.Println("start drag..")
+	}
+
+	if event == EventEndDrag {
+		ctx.state.pointerCapture = -1
+
+		log.Println("end drag..")
+	}
+
+	if ctx.state.isLastEventPointerType {
+		// Update the knob position
+		if (event == EventDragging) || (event == EventDown) {
+			// default = Horizontal
+			p1 := input.PointerPosition(0).MousePos[0]
+			p0 := bound.X
+			v = (p1 - p0)/bound.W
+
+			if v > 1 {
+				v = 1
+			}
+			if v < 0 {
+				v = 0
+			}
+		}
+	}
+	return
+}
+
+func (ctx *Context) endSlider() {
+
+}
+
+func (ctx *Context) capturePoint() {
+
+}
+
+func (ctx *Context) releasePointer() {
+
+}
+
+func (ctx *Context) isLastEventPointerType() bool {
+	return true
+}
+
 // Container: Window/PopupWindow/
 func (ctx *Context) BeginGroup() {
 
@@ -244,6 +458,37 @@ func (ctx *Context) BeginGroup() {
 
 func (ctx *Context) EndGroup() {
 
+}
+
+// Layout
+// Layout 的时候清空 offset，这样避免把上一个 Layout 的 offset 代入到下一个布局
+func (ctx *Context) BeginLayout(id ID, xtype LayoutType) {
+	lm := &ctx.Layout
+	lm.Bound.Offset.X = 0
+	lm.Bound.Offset.Y = 0
+
+	if bb, ok := lm.FindLayout(id); ok {
+		var (
+			x = lm.hGroup.X + ctx.Layout.Bound.X
+			y = lm.hGroup.Y + ctx.Layout.Bound.Y
+		)
+
+		// debug draw - render group frame
+		ctx.RenderBorder(x, y, bb.W, bb.H, 0xFF00FF00)
+		log.Println("render border:", x, y, bb.W, bb.H)
+
+
+		bb.X, bb.Y = lm.Bound.X, lm.Bound.Y
+		lm.PushLayout(xtype, bb)
+	} else {
+		lm.NewLayout(id, xtype)
+	}
+}
+
+func (ctx *Context) EndLayout() {
+	ctx.Layout.EndLayout()
+
+	//log.Println("end layout:", len(ctx.Layout.groupStack))
 }
 
 // Reference System: VirtualBounds
@@ -277,5 +522,3 @@ func init() {
 	screen.Width = 480
 	screen.Height = 320
 }
-
-type ID int
--- gui/layout.go
+++ gui/layout.go
@@ -1,6 +1,11 @@
 package gui
 
-import "github.com/go-gl/mathgl/mgl32"
+import (
+	"github.com/go-gl/mathgl/mgl32"
+	"korok.io/korok/engi/math"
+
+	"log"
+)
 
 // GUI coordinate system
 // (0, 0)
@@ -27,8 +32,31 @@ const (
 
 // 当 W = 0, H = 0 的时候，按 WrapContent 的方式绘制
 type Bound struct{
+	id ID
 	X, Y float32
 	W, H float32
+}
+
+type Cursor struct {
+	Bound
+	Offset struct{
+		X, Y float32
+	}
+	Flag uint32 // dirty flag
+
+}
+
+func (b *Bound) Offset(x, y float32) *Bound {
+	b.X, b.Y = x, y
+	return b
+}
+
+func (b *Bound) Size(w, h float32) {
+	b.W, b.H = w, h
+}
+
+func (b *Bound) SizeAuto() {
+	b.W, b.H = 0, 0
 }
 
 func (b *Bound) InRange(p mgl32.Vec2) bool{
@@ -41,132 +69,295 @@ func (b *Bound) InRange(p mgl32.Vec2) bool{
 	return true
 }
 
-type Layout struct {
+type LayoutManager struct {
 	Horizontal, Vertical Direction
-	Bound
+	Bound Cursor
 	Align
-	stack []Bound
+	// ui bound 是一直存储的，记录一些持久化的数据
+	uiElements           []Bound // element uiElements
+
+	// group 是 fifo 的结构,记录动态的数据
+	groupStack           []Group // groupStack uiElements
+	// header of group stack
+	hGroup *Group
 }
 
-// Stack Operation
-func (lyt *Layout) Begin(bb *Bound) {
-	if bb != nil {
-		lyt.stack = append(lyt.stack, *bb)
-	} else {
-		lyt.stack = append(lyt.stack, Bound{0, 0, 0, 0})
+func (lyt *LayoutManager) Initialize() {
+	// init size, todo resize 会导致指针错误
+	lyt.uiElements = make([]Bound, 0, 32)
+	lyt.groupStack = make([]Group, 0, 8)
+
+	// Create a default layout
+	bb := lyt.NewElement(0)
+	ii := len(lyt.groupStack)
+	lyt.groupStack = append(lyt.groupStack, Group{LayoutType:LinearOverLay, Bound: bb})
+	lyt.hGroup = &lyt.groupStack[ii]
+}
+
+// 创建新的Layout
+func (lyt *LayoutManager) NewElement(id ID) *Bound {
+	ii := len(lyt.uiElements)
+	lyt.uiElements = append(lyt.uiElements, Bound{id:id})
+	return &lyt.uiElements[ii]
+}
+
+// 找出前一帧保存的大小
+func (lyt *LayoutManager) Element(id ID) (bb *Bound, ok bool) {
+	if size := len(lyt.uiElements); size > int(id) {
+		if bb = &lyt.uiElements[id]; bb.id == id {
+			ok = true; return
+		}
 	}
-	lyt.Align = AlignCenter
-	lyt.Horizontal, lyt.Vertical = Left2Right, Top2Bottom
-}
-
-func (lyt *Layout) Push(bb *Bound) (id int) {
-	id = len(lyt.stack)
-	lyt.stack = append(lyt.stack, *bb)
+	// Linear Search
+	for i := range lyt.uiElements {
+		if bb = &lyt.uiElements[i]; bb.id == id {
+			ok = true; break
+		}
+	}
 	return
 }
 
-func (lyt *Layout) Pop() (bb Bound) {
-	lyt.stack = lyt.stack[:len(lyt.stack)-1]
-	return
+func (lyt *LayoutManager) Dump()  {
+	log.Println("dump elemnts:", lyt.uiElements)
+	log.Println("dump group:", lyt.groupStack)
 }
 
-func (lyt *Layout) Reset() {
-	lyt.stack = lyt.stack[:0]
+func (lyt *LayoutManager) Reset() {
+	lyt.uiElements = lyt.uiElements[:0]
 }
 
 // Bound Operation
-func (lyt *Layout) Move(x, y float32) *Layout {
-	lyt.X, lyt.Y = x, y
+func (lyt *LayoutManager) Move(x, y float32) *LayoutManager {
+	lyt.Bound.X, lyt.Bound.Y = x, y
 	return lyt
 }
 
-func (lyt *Layout) BelowOf(id int) *Layout {
-	if id < len(lyt.stack) && id >= 0{
-		bb := &lyt.stack[id]
-		lyt.Bound.Y = bb.Y + bb.H
+func (lyt *LayoutManager) BoundOf(id ID) (bb Bound, ok bool) {
+	if size := len(lyt.uiElements); size > int(id) {
+		if bb = lyt.uiElements[id]; bb.id == id {
+			ok = true
+		}
 	}
-	return lyt
+	// 否则进行线性查找, 找出UI边界
+	return
 }
 
-func (lyt *Layout) TopOf(id int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		lyt.Bound.Y = bb.Y - lyt.Bound.H
-	}
-	return lyt
-}
-
-func (lyt *Layout) LeftOf(id int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		lyt.Bound.X = bb.X - lyt.Bound.W
-	}
-	return lyt
-}
-
-func (lyt *Layout) AlignTopOf(id int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		lyt.Bound.Y = bb.Y
-	}
-	return lyt
-}
-
-func (lyt *Layout) AlignLeftOf(id int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		lyt.Bound.X = bb.X
-	}
-	return lyt
-}
-
-func (lyt *Layout) RightOf(id int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		lyt.Bound.X = bb.X+bb.W
-	}
-	return lyt
-}
-
-func (lyt *Layout) RatioH(id int, num, i, used int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		col := bb.W/float32(num)
-		lyt.Bound.X = bb.X + col * float32(i)
-		lyt.Bound.W = col * float32(used)
-	}
-	return lyt
-}
-
-func (lyt *Layout) RatioV(id int, num, i, used int) *Layout {
-	if id < len(lyt.stack) && id >= 0 {
-		bb := &lyt.stack[id]
-		col := bb.H/float32(num)
-		lyt.Bound.Y = bb.Y + col * float32(i)
-		lyt.Bound.H = col * float32(used)
-	}
-	return lyt
-}
-
-func (lyt *Layout) Offset(dx, dy float32) *Layout {
+func (lyt *LayoutManager) Offset(dx, dy float32) *LayoutManager {
 	lyt.Bound.X += dx
 	lyt.Bound.Y += dy
+
+	lyt.Bound.Offset.X = dx
+	lyt.Bound.Offset.Y = dy
 	return lyt
 }
 
-func (lyt *Layout) Size(w, h float32) *Layout{
-	lyt.W, lyt.H = w, h
+func (lyt *LayoutManager) Size(w, h float32) *LayoutManager {
+	lyt.Bound.W, lyt.Bound.H = w, h
 	return lyt
 }
 
-func (lyt *Layout) SizeAuto() *Layout {
-	lyt.W, lyt.H = 0, 0
+func (lyt *LayoutManager) SizeAuto() *LayoutManager {
+	lyt.Bound.W, lyt.Bound.H = 0, 0
 	return lyt
 }
 
-func (lyt *Layout) Flow(h, v Direction) {
+func (lyt *LayoutManager) Flow(h, v Direction) {
 	lyt.Horizontal, lyt.Vertical = h, v
 }
 
+// AutoLayout System
+func (lyt *LayoutManager) NewLayout(id ID, xtype LayoutType) {
+	bb := lyt.NewElement(id)
+	ii := len(lyt.groupStack)
+	// 默认的布局没有 parent
+	parent := &lyt.groupStack[ii-1]
+
+	lyt.groupStack = append(lyt.groupStack, Group{LayoutType:xtype, Bound: bb})
+	lyt.hGroup = &lyt.groupStack[ii]
+
+	// 启动布局之后，应该重置光标, 此时光标的位置全部相对于布局
+	// 保存父容器的当前光标位置， 当前好像没设计这个变量
+	parent.Cursor.X = lyt.Bound.X
+	parent.Cursor.Y = lyt.Bound.Y
+
+	if parent.id == 0 {
+		log.Println("parent cursor:", parent.Cursor)
+	}
+
+	// 在 EndLayout的时候再恢复Cursor位置
+
+	// 此时已经可以得出 当前Layout的位置了
+	// 好像光标的位置就是 Group 的位置
+	// 那么其实完全不需要 Cursor的存在！！
+	//
+	// Q. 目前的布局使计算父容器的坐标的时候，仅仅考虑父容器的坐标
+	// 其实父容器还有自己的父容器，需要把所有父容器的本身相对其父容器的偏移加起来才是对的！！
+	g := lyt.hGroup
+
+	// 先加上父父容器
+	g.X, g.Y = parent.X, parent.Y
+	// 再加上当前父容器的偏移, 这样得到的容器坐标就是绝对的坐标
+	g.X, g.Y = g.X+lyt.Bound.X , g.Y+lyt.Bound.Y
+
+	// 记录光标的偏移...
+	// EndLayout的时候还需要这个数据..
+	g.Offset = lyt.Bound.Offset
+
+	lyt.Bound.X, lyt.Bound.Y = 0, 0
+}
+
+func (lyt *LayoutManager) FindLayout(id ID) (bb *Bound, ok bool) {
+	return lyt.Element(id)
+}
+
+// set as current layout
+// 这个方法的逻辑和 NewLayout 都是重复的
+func (lyt *LayoutManager) PushLayout(xtype LayoutType, bb *Bound) {
+	ii := len(lyt.groupStack)
+	// 默认的布局没有 parent
+	// 此处可以安全的取出 parent，因为初始化的时候会先创建出 parent 布局..
+	parent := &lyt.groupStack[ii-1]
+	lyt.groupStack = append(lyt.groupStack, Group{LayoutType:xtype, Bound: bb})
+	lyt.hGroup = &lyt.groupStack[ii]
+
+	// 启动布局之后，应该重置光标, 此时光标的位置全部相对于布局
+	// 保存父容器的当前光标位置， 当前好像没设计这个变量
+	parent.Cursor.X = lyt.Bound.X
+	parent.Cursor.Y = lyt.Bound.Y
+
+	log.Println("push layout parent:", parent.id, " layout:", bb.id)
+	log.Println("element-0 id:", lyt.uiElements[0].id, "stack-0:", lyt.groupStack[0].id, " stack size:",ii)
 
 
+	// 在 EndLayout的时候再恢复Cursor位置
+
+	// 此时已经可以得出 当前Layout的位置了
+	// 好像光标的位置就是 Group 的位置
+	// 那么其实完全不需要 Cursor的存在！！
+	//
+	// Q. 目前的布局使计算父容器的坐标的时候，仅仅考虑父容器的坐标
+	// 其实父容器还有自己的父容器，需要把所有父容器的本身相对其父容器的偏移加起来才是对的！！
+	g := lyt.hGroup
+
+	// 先加上父父容器
+	g.X, g.Y = parent.X, parent.Y
+
+	// 再加上当前父容器的偏移, 这样得到的容器坐标就是绝对的坐标
+	g.X, g.Y = g.X+lyt.Bound.X , g.Y+lyt.Bound.Y
+
+
+	lyt.Bound.X, lyt.Bound.Y = 0, 0
+}
+
+// 重新计算父容器的大小
+// bb (offsetX, offsetY, Width, Height)
+func (lyt *LayoutManager) Extend(bb *Bound) {
+	if g := lyt.hGroup; g != nil {
+		switch g.LayoutType {
+		case LinearHorizontal:
+			// 水平加之，高度取最大
+
+			log.Println("extend h:", g.Size.W, " add:", bb.W + bb.X)
+
+			g.Size.W += bb.W + bb.X
+			g.Size.H = math.Max(g.Size.H, bb.H+bb.Y)
+		case LinearVertical:
+			// 高度加之，水平取最大
+
+			log.Println("extend v:", g.Size.H, " add:", bb.H + bb.Y)
+
+			g.Size.W = math.Max(g.Size.W, bb.W+bb.X)
+			g.Size.H += bb.H + bb.Y
+		case LinearOverLay:
+			// 重叠, 取高或者宽的最大值
+			g.Size.W = math.Max(g.Size.W, bb.W+bb.X)
+			g.Size.H = math.Max(g.Size.H, bb.H+bb.Y)
+		}
+	}
+}
+
+// 重新计算父容器的光标位置
+func (lyt *LayoutManager) Advance(bb *Bound) {
+	if g := lyt.hGroup; g != nil  {
+		cursor := &lyt.Bound
+		switch g.LayoutType {
+		case LinearHorizontal:
+			// 水平步进，前进一个控件宽度
+			cursor.X += bb.W
+		case LinearVertical:
+			// 垂直步进，前进一个控件高度
+			cursor.Y += bb.H
+		case LinearOverLay:
+			// 保持原来的位置不变..
+		}
+	}
+}
+
+func (lyt *LayoutManager) NextBound(sq int) Bound {
+	return lyt.uiElements[sq]
+}
+
+// 需要重新调整光标位置，
+// 如果上一层有 Layout，则应该按照该Layout的布局方式移动光标
+// 如果没有，则回到Group开始的位置
+// 可以在 Group 里面插入一个 默认的 Group 这样永远有一个 Group 存在
+func (lyt *LayoutManager) EndLayout() {
+	// 1. 计算并更新Layout的大小
+	// g := lyt.hGroup
+	// 在计算 UIElement 的时候已经计算过了（或许应该放在此处计算）
+	size := lyt.hGroup.Size
+	// 记录这一帧的值，清空计数器
+	// 或者应该把计数器和帧值区分开来
+	lyt.hGroup.W = size.W
+	lyt.hGroup.H = size.H
+
+	// 2. Layout出栈, 此时可以保证此处总是大于1，否则应该报错
+	if size := len(lyt.groupStack); size > 1 {
+		lyt.groupStack = lyt.groupStack[:size-1]
+		lyt.hGroup = &lyt.groupStack[size-2]
+	}
+
+	// 3. 处理父容器的布局,
+	// 先回到 Group 的位置（因为进入子容器的时候重置了Cursor，所以要先回复）, 然后才可以布局
+	g := lyt.hGroup
+	lyt.Bound.X, lyt.Bound.Y = g.Cursor.X, g.Cursor.Y
+
+	if g.LayoutType == LinearHorizontal {
+		log.Println("back to horizontal, vsize:", size)
+	}
+
+	// 2. 扩展父容器大小
+	lyt.Extend(&Bound{0, 0, 0, size.W, size.H})
+
+	// 2. 接下来直接用 Advance 步进光标
+	lyt.Advance(&Bound{0, 0,0, size.W, size.H})
+
+
+
+	if g.LayoutType == LinearHorizontal {
+		log.Println("back to horizontal, resize:", lyt.hGroup.H)
+	}
+}
+
+// Q. 当前 Group 的 X，Y, W, H 应该和 Group 的Cursor区分开来
+
+type Flag uint32
+
+type Group struct {
+	LayoutType; Flag
+	*Bound
+	// 仅用来缓存...
+	Cursor struct{X, Y float32}
+	Offset struct{X, Y float32}
+
+	// 当前帧布局的计算变量
+	Size struct{W, H float32}
+}
+
+// 布局算法：
+// 开始布局, 查找 UIElement 的大小，如果
+// 有大小则计算布局
+//    布局Group + Cursor + Layout-Weight
+// 否则创建一个新的UIElement, 如果是 Group
+// 则在EndLayout的时候计算出代销，如果Element
+// 则直接算出Layout
